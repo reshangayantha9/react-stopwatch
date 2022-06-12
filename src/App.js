dcccec962e8e6a107d9125f65f2afe7d3fd4e165
reshangayantha9 <reshangayantha9@gmail.com>
2022-06-12 16:13:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const Header = () => {
+  return <h1 className="heading">STOP WATCH</h1>;
+};
+
+const Timer = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="timer">
+      <div className="hour">{props.time.hour}</div>:
+      <div className="minute">{props.time.minute}</div>:
+      <div className="second">{props.time.second}</div>:
+      <div className="millisecond">{props.time.millisecond}</div>
     </div>
   );
+};
+
+const Action = (props) => {
+  return (
+    <div className="action">
+      <button onClick={props.handleReset}id='reset'>RESET</button>
+      <button onClick={props.handleStart}id='start'>START</button>
+      <button onClick={props.handleStop}id='stop'>STOP</button>
+    </div>
+  );
+};
+
+class App extends React.Component {
+  constructor(props, timer) {
+    super(props);
+    this.timer = timer;
+    this.state = {
+      hour: 0,
+      minute: 0,
+      second: 0,
+      millisecond: 0,
+    };
+  }
+  handleStart = () => {
+    this.timer=setInterval(()=>{
+      this.setState((prevState)=>{
+        return{
+          millisecond:prevState.millisecond+1
+        }
+      });
+      if(this.state.millisecond === 100){
+        this.setState((prevState)=>{
+          return{
+            second:prevState.second+1,
+            millisecond:0
+          }
+        })
+      }
+      if(this.state.second ===60){
+        this.setState((prevState)=>{
+          return{
+            minute:prevState.minute+1,
+            second:0
+          }
+        })
+      }
+      if(this.state.minute ===60){
+        this.setState((prevState)=>{
+          return{
+            hour:prevState.hour+1,
+            minute:0
+          }
+        })
+      }
+    },10)
+  };
+
+  handleStop = () => {
+    clearInterval(this.timer);
+  };
+
+  handleReset = () => {
+    this.setState(() => {
+      return {
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="parent">
+        <div className="main">
+        <Header />
+        <Timer time={this.state}/>
+        <Action
+          handleStart={this.handleStart}
+          handleStop={this.handleStop}
+          handleReset={this.handleReset}
+        />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
